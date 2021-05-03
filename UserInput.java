f6ece90bbd97a0073d95b4668bfb3bb26e42ce59
Troy Drescher <>
2021-05-03 19:29:33 +0000

    stuff

--- UserInput.java
+++ UserInput.java
@@ -1,63 +1,51 @@
 import java.util.Scanner;
 
+class UserInput {
+  private Scanner kb;
 
-class UserInput 
-{
-    private Scanner kb;
-    
-  public UserInput()
-  {
+  public UserInput() {
     kb = new Scanner(System.in);
   }
-  
-  public String getAggie()
-  {
+
+  public String getAggie() {
     String move = "";
     boolean valid = false;
-    while(!valid)
-    {
+    while (!valid) {
       System.out.println("Press 1,2,3,4,5,6,7, or 8");
       move = kb.nextLine();
-      
-    if(move.equals("69"))
-      {
-          java.lang.System.exit(0);
-          return null;
-          
-      }else if(move.equals("1") || move.equals("2") || move.equals("3") || move.equals("4") || move.equals("5") || move.equals("6")|| move.equals("7")|| move.equals("8"))
-    {    
+
+      if (move.equals("69")) {
+        java.lang.System.exit(0);
+        return null;
+
+      } else if (move.equals("1") || move.equals("2") || move.equals("3") || move.equals("4") || move.equals("5")
+          || move.equals("6") || move.equals("7") || move.equals("8")) {
         valid = true;
-    }else
+      } else
         System.out.println("*** INVALID INPUT ***");
     }
     return move;
   }
-  
-  
-  public String getMove()
-  {
+
+  public String getMove() {
+
     String move = "";
     boolean valid = false;
-    while(!valid)
-    {
+    while (!valid) {
       System.out.println("Press 1,2,3 or 4");
       move = kb.nextLine();
-      
-    if(move.equals("6"))
-      {
-          java.lang.System.exit(0);
-          return null;
-          
-      }else if(move.equals("1") || move.equals("2") || move.equals("3") || move.equals("4"))
-    {    
+
+      if (move.equals("6")) {
+        java.lang.System.exit(0);
+        return null;
+
+      } else if (move.equals("1") || move.equals("2") || move.equals("3") || move.equals("4")) {
         valid = true;
-    }else
+      } else
         System.out.println("*** INVALID INPUT ***");
     }
+
     return move;
   }
-  
-  
-  
-  
+
 }
